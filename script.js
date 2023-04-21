16c456aa0456672aba6e8d1241e7bf38e31f1c75
Nikita Fomichev <nickita.fomichev2016@yandex.ru>
2023-04-21 05:24:52 +0300

    mend

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-const addButton = document.querySelector('.profile__add-button');
-
-addButton.addEventListener('click', () => {
-  const popup = document.querySelector('.popup');
-  popup.classList.add('popup_opened');
-});
-
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__button-close');
+const nameProfile = document.querySelector('.profile__title');
+const descriptionProfile = document.querySelector('.profile__subtitle');
+const saveButton = document.querySelector('.popup__button-save');
 
-closeButton.addEventListener('click', () => {
-  const popup = document.querySelector('.popup');
+//get text for value inputs
+const popupName = document.querySelector('#popup__name');
+popupName.value = nameProfile.textContent;
+const popupDescription = document.querySelector('#popup__description');
+popupDescription.value = descriptionProfile.textContent;
+
+function openPopup() {
+  popup.classList.add('popup_opened');
+}
+
+function closePopup() {
   popup.classList.remove('popup_opened');
-});
+}
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  nameProfile.textContent = popupName.value;
+  descriptionProfile.textContent = popupDescription.value;
+  closePopup();
+}
+
+
+saveButton.addEventListener('click', handleFormSubmit);
