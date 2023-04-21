65d302f901e89acebda1c90447b9ff2fe11886b1
Nikita Fomichev <nickita.fomichev2016@yandex.ru>
2023-04-21 20:13:20 +0300

    mend

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__button-close');
 const nameProfile = document.querySelector('.profile__title');
 const descriptionProfile = document.querySelector('.profile__subtitle');
-const saveButton = document.querySelector('.popup__button-save');
+const elementForm = document.querySelector('.popup__container');
+const popupName = document.querySelector('#popup__name');
+const popupDescription = document.querySelector('#popup__description');
 
 //get text for value inputs
-const popupName = document.querySelector('#popup__name');
 popupName.value = nameProfile.textContent;
-const popupDescription = document.querySelector('#popup__description');
 popupDescription.value = descriptionProfile.textContent;
 
 function openPopup() {
@@ -31,4 +31,4 @@ function handleFormSubmit(evt) {
 }
 
 
-saveButton.addEventListener('click', handleFormSubmit);
+elementForm.addEventListener('submit', handleFormSubmit);
