2699f341abf2fc0e5dd7b1b757d0bf52309a2bee
Nikita Fomichev <nickita.fomichev2016@yandex.ru>
2023-04-22 02:30:58 +0300

    feat: add-popup

--- script.js
+++ script.js
@@ -1,34 +1,54 @@
-const popup = document.querySelector('.popup');
+// init profile
 const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.popup__button-close');
-const nameProfile = document.querySelector('.profile__title');
+const addButton = document.querySelector('.profile__add-button');
+const profileName = document.querySelector('.profile__title');
 const descriptionProfile = document.querySelector('.profile__subtitle');
-const elementForm = document.querySelector('.popup__container');
-const popupName = document.querySelector('#popup__name');
-const popupDescription = document.querySelector('#popup__description');
 
-//get text for value inputs
-popupName.value = nameProfile.textContent;
-popupDescription.value = descriptionProfile.textContent;
+// init edit-popup
+const editPopup = document.querySelector('.edit-popup');
+const CloseEditButton = document.querySelector('.edit-popup__button-close');
+const elementEditForm = document.querySelector('.edit-popup__edit-form');
+const editPopupName = document.querySelector('#edit-popup__name');
+const editPopupDescription = document.querySelector('#edit-popup__description');
 
-function openPopup() {
-  popup.classList.add('popup_opened');
+// init add-popup
+const addPopup = document.querySelector('.add-popup');
+const CloseAddButton = document.querySelector('.add-popup__button-close');
+const elementAddForm = document.querySelector('.add-popup__add-form');
+const addPopupName = document.querySelector('#add-popup__form-name');
+
+//get text for value inputs in edit-popup
+editPopupName.value = profileName.textContent;
+editPopupDescription.value = descriptionProfile.textContent;
+
+function openEditPopup() {
+  editPopup.classList.add('edit-popup_opened');
 }
 
-function closePopup() {
-  popup.classList.remove('popup_opened');
+function closeEditPopup() {
+  editPopup.classList.remove('edit-popup_opened');
 }
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
+function openAddPopup() {
+  addPopup.classList.add('add-popup_opened');
+}
+
+function closeAddPopup() {
+  addPopup.classList.remove('add-popup_opened');
+}
+
+editButton.addEventListener('click', openEditPopup);
+CloseEditButton.addEventListener('click', closeEditPopup);
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
 
-  nameProfile.textContent = popupName.value;
-  descriptionProfile.textContent = popupDescription.value;
-  closePopup();
+  profileName.textContent = editPopupName.value;
+  descriptionProfile.textContent = editPopupDescription.value;
+  closeEditPopup();
 }
 
+elementEditForm.addEventListener('submit', handleFormSubmit);
 
-elementForm.addEventListener('submit', handleFormSubmit);
+addButton.addEventListener('click', openAddPopup);
+CloseAddButton.addEventListener('click', closeAddPopup);
