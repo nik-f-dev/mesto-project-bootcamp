b88eadbf044fa9a5230d847a5538fb92a09a2eed
Nikita Fomichev <nickita.fomichev2016@yandex.ru>
2023-05-19 19:21:02 +0300

    feat: добавить функциональность всех кнопок и форм с использованием сервера

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import './../pages/index.css';
 
-import { handleEditFormSubmit, handleAddFormSubmit, addPopup, profileName, descriptionProfile, editPopup, elementEditForm, } from './modal.js';
-import { openPopup, closePopupEsc } from './utils.js';
+import { handleEditFormSubmit, handleAddFormSubmit, addPopup, profileName, descriptionProfile, editPopup, elementEditForm, handleAvatarFormSubmit } from './modal.js';
+import { changeButtonName, openPopup } from './utils.js';
 import { renderCards } from './card.js';
-import { enableValidation } from './validate.js'
+import { enableValidation } from './validate.js';
+import { uploadProfile } from './api.js';
+import { avatarPopup, avatarButtonSave, addPopupButtonSave, editPopupButtonSave } from './modal.js';
 
 // init profile
 const editButton = document.querySelector('.profile__edit-button');
@@ -16,19 +18,23 @@ const editPopupDescription = document.querySelector('#edit-popup__description');
 // init add-popup
 const elementAddForm = document.querySelector('.add-popup__add-form');
 
+//init avatar
+const elementAvatarForm = document.querySelector('.avatar-popup__avatar-form');
+const avatarEditButton = document.querySelector('.profile__edit-avatar-button');
+
 //open edit-popup
 editButton.addEventListener('click', () => {
   editPopupName.value = profileName.textContent;
   editPopupDescription.value = descriptionProfile.textContent;
 
+  changeButtonName(editPopupButtonSave, 'Сохранить');
   openPopup(editPopup);
-  closePopupEsc(editPopup);
 })
 
 //open add-popup
 addButton.addEventListener('click', () => {
+  changeButtonName(addPopupButtonSave, 'Создать');
   openPopup(addPopup);
-  closePopupEsc(addPopup);
 })
 
 //save edit-form
@@ -37,6 +43,14 @@ elementEditForm.addEventListener('submit', handleEditFormSubmit);
 //save add-form
 elementAddForm.addEventListener('submit', handleAddFormSubmit);
 
+avatarEditButton.addEventListener('click', () => {
+  changeButtonName(avatarButtonSave, 'Сохранить');
+  openPopup(avatarPopup);
+});
+
+elementAvatarForm.addEventListener('submit', handleAvatarFormSubmit);
+
+uploadProfile();
 
 renderCards();
 
